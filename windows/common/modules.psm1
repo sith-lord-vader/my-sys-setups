0c3d937f45ef1ab89bf32951edb63ded135e3c5c
Abhishek Adhikari <abhiayush23@gmail.com>
2023-06-03 13:00:38 +0530

    new: Install-Fonts

--- windows/common/modules.psm1
+++ windows/common/modules.psm1
@@ -19,5 +19,33 @@ function Install-Winget {
     Write-Host "$args is now ready!"
 }
 
+function Install-Fonts {
+    # ? This part of code is taken from https://witit.blog/font-installer-script-powershell/
+    param(
+    [String]$SourceDir = "Path",
+    [String]$FileRegex = ""
+    )
+    $ShellObject = New-Object -ComObject shell.application
+    $Fonts = $ShellObject.NameSpace(0x14)
+    $FontsToInstallDirectory = $SourceDir
+    $FontsToInstall = Get-ChildItem $FontsToInstallDirectory -Recurse -Include $FileRegex
+    $Ctr = 1
+    $Total = $FontsToInstall.Count
+    ForEach ($F in $FontsToInstall){
+        $FullPath = $F.FullName
+        $Name = $F.Name
+        $UserInstalledFonts = "$ENV:USERPROFILE\AppData\Local\Microsoft\Windows\Fonts"
+        If (!(Test-Path "$UserInstalledFonts\$Name")){
+            $Fonts.CopyHere($FullPath)
+            Write-Host "[$Ctr of $Total] || Installed Font $Name...moving on!" -ForegroundColor Cyan
+        }
+        else{
+            Write-Host "[$Ctr of $Total] || Font $Name is already installed bro..." -ForegroundColor Green
+        }
+        $Ctr++
+    }
+}
+
 Export-ModuleMember -Function Reset-Env
 Export-ModuleMember -Function Install-Winget
+Export-ModuleMember -Function Install-Fonts
