82ef56f55b8149c218c2b668232c7c0a198c4579
Abhishek Adhikari <abhiayush23@gmail.com>
2023-06-03 12:50:34 +0530

    new: Reset-Env updated

--- windows/common/modules.psm1
+++ windows/common/modules.psm1
@@ -1,19 +1,21 @@
 function Reset-Env {
     param(
-    [String]$Key = "Path"
+    [String]$Key = "Path",
+    [String]$Add = ""
     )
 
     Set-Item `
         -Path (('Env:', $Key) -join '') `
         -Value ((
             [System.Environment]::GetEnvironmentVariable($Key, "Machine"),
-            [System.Environment]::GetEnvironmentVariable($Key, "User")
+            [System.Environment]::GetEnvironmentVariable($Key, "User"),
+            $Add
         ) -match '.' -join ';')
 }
 
 function Install-Winget {
-    Write-Host "$args"
-    winget install $args[0] --silent
+    Write-Host "Checking $args"
+    winget install $args --silent
     Write-Host "$args is now ready!"
 }
 
